bd6cc6171252f45d32776aff9470149c825effb5
Joseph F. Ryan <ryan.311@osu.edu>
2002-12-02 07:46:22 +0000

    Import of JRYAN/Text-ParagraphDiff-2.00 from CPAN.
    gitpan-cpan-distribution: Text-ParagraphDiff
    gitpan-cpan-version:      2.00
    gitpan-cpan-path:         JRYAN/Text-ParagraphDiff-2.00.tar.gz
    gitpan-cpan-author:       JRYAN
    gitpan-cpan-maturity:     released

--- ParagraphDiff/lib/Text/ParagraphDiff.pm
+++ ParagraphDiff/lib/Text/ParagraphDiff.pm
@@ -5,230 +5,240 @@ use warnings 'all';
 use Algorithm::Diff qw(diff);
 use HTML::Entities ();
 use POSIX qw(strftime);
-use vars qw(
-    $output $start $total_offset %highlight
-    @EXPORT @EXPORT_OK @ISA $VERSION
-);
+use Data::Dumper;
+use vars qw(@EXPORT @EXPORT_OK @ISA $VERSION);
 require Exporter;
 @EXPORT = qw(text_diff);
 @EXPORT_OK = qw(create_diff html_header html_footer);
 @ISA = qw(Exporter);
-$VERSION = "1.06";
+$VERSION = "2.00";
+
 
 sub text_diff {
-	return ((html_header(@_)).(create_diff(@_)).(html_footer()));
+    return ((html_header(@_)).(create_diff(@_)).(html_footer()));
 }
 
 sub create_diff {
-    my($old,$new,) = (shift,shift);
+    my($old,$new) = (shift,shift);
     my $opt=shift if (@_);
 
+    my $old_orig = _get_lines($old);
+    my $new_orig = _get_lines($new);
+
+    my %highlight;
     if ($opt->{plain}) {
-        $highlight{minus} = qq(b><font color="#FF0000" size="+1" );
-        $highlight{plus}  = qq(b><font color="#005500" size="+1" );
-        $highlight{end} = "/font></b";
+        $highlight{minus} = qq(<b><font color="#FF0000" size="+1"> );
+        $highlight{plus}  = qq(<b><font color="#005500" size="+1"> );
+        $highlight{end} = "</font></b>";
     }
     else {
-        $highlight{minus} = qq(span class="minus" );
-        $highlight{plus}  = qq(span class="plus" );
-        $highlight{end}   = qq(/span);
+        $highlight{minus} = qq(<span class="minus"> );
+        $highlight{plus}  = qq(<span class="plus"> );
+        $highlight{end}   = qq(</span>);
     }
 
-    $start = 1;
-    $total_offset = 0;
-    $output = "";
-
-    my @old_orig;
-    if (!ref $old) {
-        open (FILE, "$old") or die $!;
-        @old_orig = <FILE>;
-        close(FILE);
-    }
-    else {
-        @old_orig = @$old;
-    }
-
-    my @old;
-    my %old_count;
-    my $char_count=0;
-    foreach (@old_orig)
+    my (@old,@old_count);
+    foreach (@$old_orig)
     {
-        $_ = HTML::Entities::encode($_);
+        $_ = HTML::Entities::encode($_) unless exists $opt->{escape};
         my @words = (/\S+/g);
-        $char_count += scalar(@words);
         push @old, @words;
-        $old_count{$char_count} = 1;
+        push @old_count, scalar(@words);
 
     }
 
-    my @new_orig;
-    if (!ref $new) {
-        open (FILE, "$new") or die $!;
-        @new_orig = <FILE>;
-        close(FILE);
-    }
-    else {
-        @new_orig = @$new;
-    }
-
-    my @new;
-    my @new_count;
-    my @space;
-    foreach (@new_orig)
+    my ($total_diff, @new, @leading_space, @count);
+    foreach (@$new_orig)
     {
-        my ($leading_white) = /( *)/;
-        push @space, $leading_white;
+        my ($leading_white) = /^( *)/;
+        push @leading_space, $leading_white;
 
-        $_ = HTML::Entities::encode($_);
+        $_ = HTML::Entities::encode($_) unless exists $opt->{escape};
         my @words = (/\S+/g);
 
+        push @$total_diff, map { [' ',$_] } @words;
         push @new, @words;
-        push @new_count, scalar(@words);
+        push @count, scalar(@words);
     }
 
+    my ($plus,$minus) = _get_diffs(\@old, \@new, \@old_count);
 
-    my @diffs = diff(\@old, \@new);
-    my @starts = get_starts(\@diffs,\%old_count);
+    _merge_plus  ($total_diff, $plus );
+    _merge_minus ($total_diff, $minus );
+    _merge_white ($total_diff, \@leading_space);
 
-    my $line_diff = 0;
-    my $last = 0;
+    $total_diff = _merge_lines ($total_diff, \@count);
 
-    foreach my $hunk (@diffs) {
-        foreach my $line (@$hunk) {
-            my $minus=0;
-            my $start_index;
-            if ($line->[0] eq '+') {
-                $start_index = $line->[1];
-                ($start_index,$line_diff) = ($start_index-$line_diff,$start_index);
-                $start = 0;
-                ($last) = print_para($last, \@new_count,\@space,@new[0..$start_index-1]);
-            }
-            elsif ($line->[0] eq '-') {
-                my $start_from = shift @starts;
-                $start_index = $start_from->[0];
-                ($start_index,$line_diff) = ($start_index-$line_diff,$start_index);
-                ($last) = print_para($last, \@new_count,\@space,@new[0..$start_index-1]);
-                $start = $start_from->[1];
-            }
+    _fold ($total_diff);
 
-            @new = @new[$start_index..$#new];
-
-            if ($line->[0] eq '+') {
-                while (!$new_count[0])
-                {
-                    shift @new_count;
-                    $output .= "<br>\n";
-                    $space[0] =~ s/\s/&nbsp;/g;
-                    $output .= (shift @space);
-                }
-                $new_count[0]--;
-                $last = output_item($last,1,"plus",$line->[2]);
-            }
-            else {
-                $last = output_item($last,2,"minus",$line->[2]);
-            }
-
-        }
-    }
-
-    print_para($last, \@new_count,\@space,@new);
-
-    $output =~ s/\Q<$highlight{end}>\E//i;
+    my $output = _format ($total_diff,\%highlight);
     return $output;
 }
 
-sub print_para {
-    my($last,$countref,$spaceref,@words) = @_;
+#########
+# Utility
 
-    ($start) ? $start = 0 : shift @words;
-    $start=0;
-
-    if (@words) {
-        $output .= "<$highlight{end}> ";
-        $last=0;
-    }
-
-    foreach my $word (@words) {
-        if ($countref->[0]) {
-            $countref->[0]--;
-            $output .= ($word . " ");
-        }
-        else {
-            while (!$countref->[0])
-            {
-                shift @$countref;
-                $output .=  "<br>\n";
-                $spaceref->[0] =~ s/\s/&nbsp;/g;
-                $output .= (shift @$spaceref);
-            }
-            $countref->[0]--;
-            $output .= ($word . " ");
-        }
-    }
-    return ($last);
-}
-
-sub get_starts {
-    my ($diffs,$para) = @_;
-    my @starts;
-    my $start_index = 0;
-    my $minus_count = 0;
-    foreach my $hunk (@$diffs) {
-        my $pos = 0;
-
-        foreach my $line (@$hunk) {
-            if ($line->[0] eq '+') {
-                $pos++;
-                last
-            }
-        }
-        if ($pos) {
-            foreach my $line (@$hunk) {
-
-                if ($line->[0] eq '+') {
-                    $start_index = $line->[1];
-                    while ($minus_count) {
-                        push @starts, [$start_index,0];
-                        $minus_count--;
-                    }
-                }
-                else {
-                    $line->[2] = $line->[2] if ($para->{$line->[1]});
-                    $minus_count++;
-                }
-            }
-        }
-        else {
-	        if (@$hunk) {
-	        	$hunk->[0][2] = "<br>".$hunk->[0][2] if ($para->{$hunk->[0][1]});
-        	}
-            foreach my $line (@$hunk) {
-                $line->[2] = "<br>".$line->[2] if ($para->{$line->[1]});
-                push @starts, [$line->[1]-$total_offset,1];
-                $total_offset++
-            }
-        }
-    }
-    return @starts;
-}
-
-sub output_item {
-
-    my ($last,$value,$type,$item) = @_;
-    if ($last) {
-        if ($last == $value) {
-            $output .=  (" " . $item);
-        }
-        else {
-            $output .= qq(<$highlight{end}>&nbsp;<$highlight{$type}>$item);
-            $last=$value;
-        }
+sub _get_lines {
+    my ($file) = @_;
+    my @lines;
+    if (!ref $file) {
+        open (FILE, "$file") or die "Can't open file $file: $!";
+        @lines = <FILE>;
+        close(FILE);
+        return \@lines;
     }
     else {
-        $output .= qq(<$highlight{$type}>$item);
-        $last=$value;
+        return $file;
     }
-    return $last;
+}
+
+sub _fold {
+    my ($diff) = @_;
+
+    foreach (@$diff) {
+        my $i = 0;
+        while ($i+1 < @$_) {
+            if ($_->[$i][0] eq $_->[$i+1][0]) {
+                my $item = splice @$_, $i+1, 1;
+                $_->[$i][1] .= (" ".$item->[1]);
+                next;
+            }
+            $i++;
+        }
+    }
+}
+
+sub _get_diffs {
+    my @diffs = diff($_[0], $_[1]);
+    my ($plus,$minus);
+    foreach my $hunk (@diffs) {
+        foreach (@$hunk) {
+            push @$plus,  $_ if $_->[0] eq '+';
+            push @$minus, $_ if $_->[0] eq '-';
+        }
+    }
+
+    _fix_minus ($minus, $_[2]);
+
+    return ($plus,$minus);
+}
+
+sub _fix_minus {
+
+    my ($i,$x) = (0,0);
+    foreach my $break (@{$_[1]}) {
+        $i += $break;
+        while (($x< @{$_[0]}) && ($i > $_[0]->[$x][1])) { $x++ }
+        last unless @{$_[0]} > $x;
+        $_[0]->[$x-1][2] .= " </p><p>" if (($i-1) == $_[0]->[$x-1][1]);
+        $x++;
+    }
+}
+
+#########
+# Merging
+
+sub _merge_plus {
+    my ($total_diff, $plus_diff) = @_;
+
+    while ( my $cur = shift @$plus_diff ) {
+        $total_diff->[$cur->[1]][0] = '+';
+    }
+}
+
+
+sub _merge_minus {
+    my ($total_diff, $min_diff) = @_;
+    my ($pos,$offset) = (0,0);
+
+    while ( my $cur = shift @$min_diff ) {
+        while ($pos < ($cur->[1]+$offset)) {
+            ++$offset if $total_diff->[$pos][0] eq '+';
+            ++$pos;
+        }
+        while ($total_diff->[$pos][0] eq '+') {
+            ++$offset;
+            ++$pos;
+        }
+        splice @$total_diff, $pos, 0, ['-',$cur->[2]];
+    }
+}
+
+sub _merge_white {
+    my ($total_diff, $whitespace) = @_;
+    my $pos = 0;
+
+    while ( @$whitespace ) {
+        my $cur = shift @$whitespace;
+        while ( $total_diff->[$pos][0] ne '-' ) { $pos++ }
+        $total_diff->[$pos][1] = $cur . $total_diff->[$pos][1];
+        $pos++;
+    }
+}
+
+sub _merge_lines {
+    my ($total_diff, $count) = @_;
+    my $new = [];
+
+    foreach my $cur ( @$count ) {
+
+        if ($cur > 0) {
+            push @$new, [];
+            my ($pos,$neg,$x) = (0)x3;
+            while ( $pos < $cur ) {
+                $pos++ if $total_diff->[$x][0] ne '-';
+                $neg++ if $total_diff->[$x][0] eq '-';
+                $x++;
+            }
+            $new->[-1] = [splice @$total_diff,0,$pos+$neg];
+        }
+    }
+    return $new;
+}
+
+
+sub _merge_old_lines {
+    my ($total_diff, $count) = @_;
+    my $new = [];
+
+    foreach my $cur ( @$count ) {
+        if ($cur > 0) {
+            push @$new, [];
+            my ($pos,$neg,$x) = (0)x3;
+            while ( $pos < $cur ) {
+                $pos++ if $total_diff->[$x][0] ne '-';
+                $neg++ if $total_diff->[$x][0] eq '-';
+                $x++;
+            }
+            $new->[-1] = [splice (@$total_diff,0,$pos+$neg)];
+        }
+    }
+    return $new;
+}
+
+#########
+# Output
+
+sub _format {
+    my ($diff,$highlight) = @_;
+    my $output;
+    foreach my $hunk (@$diff) {
+
+        $output .= "\n<p>\n";
+        foreach my $sect (@$hunk) {
+            if ($sect->[0] eq ' ') {
+                $output .= "$sect->[1] ";
+            }
+            elsif ($sect->[0] eq '+') {
+                $output .= " $highlight->{plus}$sect->[1]$highlight->{end} ";
+            }
+            else {
+                $output .= " $highlight->{minus}$sect->[1]$highlight->{end} ";
+            }
+        }
+        $output .= "\n</p>\n";
+    }
+    return $output;
 }
 
 sub html_header {
@@ -249,12 +259,12 @@ sub html_header {
     }
 
     my $header = $opt->{header} || qq(
-        <p>
+        <div>
         <font size="+2"><b>Difference of:</b></font>
         <table border="0" cellspacing="5">
         <tr><td class="minus">---</td><td class="minus"><b>$old</b></td><td>$old_time</td></tr>
         <tr><td class="plus" >+++</td><td class="plus" ><b>$new</b></td><td>$new_time</td></tr>
-        </table></p>
+        </table></div>
     );
 
     my $script = ($opt->{functionality}) ? "" : qq(
@@ -318,7 +328,7 @@ sub html_header {
         <style>
             .plus{background-color:#00BBBB; visibility="visible"}
             .minus{background-color:#FF9999; visibility="visible"}
-            P{ margin:50px; border:solid; background-color:#F2F2F2; padding:5px; }
+            DIV{ margin:50px; border:solid; background-color:#F2F2F2; padding:5px; }
             BODY{line-height:1.7; background-color:#888888}
             B{font-size:bigger;}
             .togglep {
@@ -337,50 +347,51 @@ sub html_header {
     );
 
     my $functionality = ($opt->{functionality}) ? "" : qq(
+        <div>
         <form>
-        <p>
         <table border="0" cellspacing="5">
         <td><input type="button" class="togglep" value="Toggle Plus" onclick="toggle_plus(); return false;" /></td><td width="10">&nbsp;</td>
         <td><input type="button" class="togglem" value="Toggle inus" onclick="toggle_minus(); return false;" /></td><td width="10">&nbsp;</td>
         </table>
-        </p>
         </form>
+        </div>
     );
 
     return qq(
-
-        <?xml version="1.0" encoding="iso-8859-1"?>
         <!DOCTYPE html
             PUBLIC "-//W3C//DTD XHMTL 1.0 Transitional//EN"
             "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-        <html xmlns="http://www.w3.org/1999/xhtml" lang="en-US"><head>
+        <html><head>
         <title>Difference of $old, $new</title>
         $script
         $style
         </head><body>
         $header
         $functionality
-        <p>
+        <div>
     );
 }
 
-sub html_footer { return "</p></body></html>" }
+sub html_footer { return "</div></body></html>" }
 
 1;
+
 __END__
+
+=pod
 
 =head1 NAME
 
-Text::DiffParagraph - Visual Difference for paragraphed text.
+Text::ParagraphDiff - Visual Difference for paragraphed text.
 
 =head1 ABSTRACT
 
-C<Text::DiffParagraph> finds the difference between two paragraphed text files by word
+C<Text::ParagraphDiff> finds the difference between two paragraphed text files by word
 rather than by line, reflows the text together, and then outputs result as xhtml.
 
 =head1 SYNOPSIS
 
-    use Text::DiffParagraph;
+    use Text::ParagraphDiff;
     print text_diff($old,$new);            # $old and $new are filenames
     print text_diff(\@old,\@new);          # Or pass array references
     print text_diff($old,$new,{plain=>1}); # Pass options (see below)
@@ -390,9 +401,9 @@ rather than by line, reflows the text together, and then outputs result as xhtml
 
 =head1 DESCRIPTION
 
-C<Text::DiffParagraph> is a reimplementation of C<diff> that is meant for
+C<Text::ParagraphDiff> is a reimplementation of C<diff> that is meant for
 paragraphed text rather than for code.  Instead of "diffing" a document by
-line, C<Text::DiffParagraph> expands a document to one word per line, uses
+line, C<Text::ParagraphDiff> expands a document to one word per line, uses
 C<Algorithm::Diff> to find the difference, and then reflows the text back
 together, highlighting the "add" and "subtract" sections.  Writers and editors
 might find this useful for sending revisions to each other across the internet;
@@ -401,7 +412,7 @@ please see diff.html in the distribution, as well as the sources for the
 difference, old.txt and new.txt.
 
 The output is in xhtml, for ease of generation, ease of access, and ease of
-viewing.  C<Text::DiffParagraph> also takes advantage of two advanced features
+viewing.  C<Text::ParagraphDiff> also takes advantage of two advanced features
 of the median: CSS and JavaScript.
 
 CSS is used to cut down on output size and to make the output very pleasing to
@@ -416,6 +427,7 @@ Options are stored in a hashref, C<$opt>.  C<$opt> is an optional last argument
 to C<text_diff>, passed like this:
 
     text_diff($old, $new, {plain => 1,
+                           escape => 1,
                            functionality => 1,
                            style => 'stylesheet_code_here',
                            header => 'header_markup_here'});
@@ -440,11 +452,20 @@ buttons to not be shown.
 When C<style> is set, its value will override the default stylesheet.  Please
 see C<output_html_header> above for the default stylesheet specifications.
 
+=item B<header>
+
+When C<header> is set, its value will override the default difference header.
+Please see C<output_html_header> above for more details.
 
 =item B<header>
 
 When C<header> is set, its value will override the default difference header.
 Please see C<output_html_header> above for more details.
+
+=item B<escape>
+
+When C<escape> is set, then output will not be escaped.  Useful if you want to
+include your own markup.
 
 =back
 
@@ -452,7 +473,7 @@ Please see C<output_html_header> above for more details.
 
 C<text_diff> is exported by default.
 Additionally, C<create_diff>, C<html_header>, and C<html_footer> are optionally
-exported by request (e.g. use Text::DiffParagraph qw(create_diff)).
+exported by request (e.g. use C<< Text::ParagraphDiff qw(create_diff)) >>.
 C<create_diff> is the actual diff itself; C<html_header> and C<html_footer>
 should be obvious.
 
